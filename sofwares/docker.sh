6659ef822540aa8bba59da88e59a84e34eacbac7
hariananthagiri <hariananthagiri@gmail.com>
2024-09-28 06:16:34 +0530

    jenkins

---
+++ sofwares/docker.sh
@@ -0,0 +1,7 @@
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo systemctl status docker
+sudo usermod -aG docker ubuntu
---
+++ sofwares/minikube.sh
@@ -0,0 +1,50 @@
+*1. Remove any existing*
+---------------------
+
+
+sudo yum remove -y docker-ce docker-ce-cli
+
+sudo yum remove -y yum-utils
+
+*2. Fresh Docker Installation*
+-----------------------
+From the link : https://docs.docker.com/engine/install/centos/
+
+sudo yum install -y yum-utils
+
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+
+Start Docker:
+------------
+
+sudo systemctl start docker
+
+To check if Docker running : docker ps
+
+*3. Steps to install MInikube on Docker Container*
+--------------------------------------------------
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+
+minikube start --driver=docker
+
+minikube config set driver docker
+
+
+*4. Validate Kubernetes by running Kubectl commands*
+-------------------------------------------------
+
+Kubectl get ns or any kubectl commands
+
+# installing kubectl
+
+ curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+ chmod +x ./kubectl
+ sudo mv ./kubectl /usr/local/bin/kubectl
+
